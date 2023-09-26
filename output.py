9a467e1bc271a7efcb941e61c2a7b85615b12a6f
JiruSang <janicesang66@gmail.com>
2023-09-26 21:17:11 +1000

    add comments to code

--- output.py
+++ output.py
@@ -1,29 +1,45 @@
+# Import necessary modules
 import sys
 import logger
 import utils
 from Solution import SolutionAgent
 
+# Check if the script is being run as the main program
 if __name__ == '__main__':
+    # Initialize variables to store file path and grid string
     words_file_path = ''
     grid_string = ''
-    if len(sys.argv) == 3:
-        words_file_path = sys.argv[1]
-        grid_string = sys.argv[2]
 
+    # Check if the correct number of command-line arguments (2) are provided
+    if len(sys.argv) == 3:
+        words_file_path = sys.argv[1]  # The first argument is the words file path
+        grid_string = sys.argv[2]      # The second argument is the grid string
     else:
         print("Wrong number of arguments")
+
     try:
         word_grid = grid_string
+
+        # Check if the grid string has exactly 16 characters
         if len(word_grid) != 16:
             print("Wrong number of characters")
+        # Check if the grid string contains only alphabetic characters
         elif not word_grid.isalpha():
             print("Character should only be alphabet")
         else:
+            # Clean the original grid string
             original_grid = utils.clean_string(word_grid)
+
+            # Convert the cleaned grid string to a structured grid
             structured_grid = utils.convert_to_structured(original_grid)
+
+            # Initialize a solution agent
             solution_agent = SolutionAgent()
 
-            check_strings = utils.read_file_in(file_path = words_file_path)
+            # Read words from the specified file
+            check_strings = utils.read_file_in(file_path=words_file_path)
+
+            # Iterate through the words and check if they exist in the grid
             for word in check_strings:
                 if solution_agent.exist(structured_grid, word):
                     print(word)
--- utils.py
+++ utils.py
@@ -1,28 +1,47 @@
-
-
+# Function to clean a word by removing punctuation and converting it to uppercase
 def clean_string(word):
+    # Define a string containing punctuation characters to remove
     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    # Iterate through each character in the word
     for ele in word:
+        # Check if the character is in the punctuation string
         if ele in punc:
+            # If it's a punctuation character, replace it with an empty string
             word = word.replace(ele, "")
+
+    # Convert the cleaned word to uppercase and return it
     return word.upper()
 
+
+# Function to convert a string into a 2D array of specified dimensions (4x4)
 def convert_to_structured(word):
+    # Initialize an empty 4x4 2D array
     array_2d = [[' ' for _ in range(4)] for _ in range(4)]
 
     # Populate the 2D array row by row
     index = 0
     for i in range(4):
         for j in range(4):
+            # Assign characters from the input word to the 2D array
             array_2d[i][j] = word[index]
             index += 1
+
+    # Return the populated 2D array
     return array_2d
 
+
+# Function to read words from a file and store them in a list
 def read_file_in(file_path):
+    # Initialize an empty list to store the words
     words = []
 
+    # Open the specified file in read mode
     with open(file_path, "r") as file:
+        # Iterate through each line in the file
         for line in file:
+            # Remove leading and trailing whitespace from each line and add it to the list
             words.append(line.strip())
 
+    # Return the list of words
     return words
